2b7c2a346735a21a6b1c786e69a9533ac60ff090
asmi2 <asmi24886@gmail.com>
2025-02-24 23:39:35 +0530

    Immutable class pattern added

---
+++ src/main/java/com/practice/interview/immutables/ImmutableEmployee.java
@@ -0,0 +1,29 @@
+package com.practice.interview.immutables;
+
+import java.util.Collections;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public final class ImmutableEmployee {
+    private final int i;
+    private final String s;
+    private final Map<Integer, String> m;
+
+    public ImmutableEmployee(int i, String s, Map<Integer, String> m) {
+        this.i = i;
+        this.s = s;
+        this.m = cloneMap(m);
+    }
+
+    public int getI() { return i; }
+    public String getS() { return s; }
+    public Map<Integer, String> getM() { return cloneMap(m); }
+
+    public static <K, V> Map<K, V> cloneMap(Map<K, V> map) {
+        return Collections.unmodifiableMap(
+                map.entrySet().stream().collect(
+                        Collectors.toMap(Map.Entry<K, V>::getKey, Map.Entry<K, V>::getValue)
+                )
+        );
+    }
+}
