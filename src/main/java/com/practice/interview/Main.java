80e42aa45142dda817abcc8ebff892e3aa4bfaac
asmi2 <asmi24886@gmail.com>
2025-03-06 10:53:44 +0530

    Added some conversion examples

--- src/main/java/com/practice/interview/Main.java
+++ src/main/java/com/practice/interview/Main.java
@@ -5,6 +5,16 @@ import java.util.LinkedList;
 import java.util.stream.IntStream;
 
 public class Main {
+
+    /*
+
+            Things to look into:
+            1. Integer to int
+            2. int to Integer
+            3. Integer to Character
+            4. Character to Integer
+            5. Character to String
+     */
     public static void main(String[] args) {
         String input = "aabbcdeeeff";
 
@@ -40,6 +50,27 @@ public class Main {
         /********************************************************/
 
         new String(charArr).chars().mapToObj(i -> (char) i).forEach(System.out::println);
+    }
+
+
+    public void data_type_transform() {
+        //Integer to int And back -> this can also be auto boxed and unboxed
+        Integer a = Integer.valueOf(10);
+        int b = a.intValue();
+
+        //char To Character And back -> this can also be auto boxed and unboxed
+        Character charA = Character.valueOf('z');
+        char charB = Character.valueOf(charA);
+
+        //Integer to Character And back
+        Integer aa = 11;
+        char charAA = (char) aa.intValue();
+        Character charBB = (char) aa.intValue();
+
+        int cc = Character.getNumericValue(charAA);
+
+        String s = String.valueOf(charBB);
+
     }
 
 }
